c3041f03ef85f5e2f781b0bea3e21c234e23e152
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-09-18 00:07:11 -0300

    feat: creating aside content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { Github } from 'lucide-react'
+import { FileVideo, Github, Upload, Wand2 } from 'lucide-react'
 import { Button } from './components/ui/button'
 import { Separator } from './components/ui/separator'
 import { Textarea } from './components/ui/textarea'
+import { Label } from './components/ui/label'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/select'
+import { Slider } from './components/ui/slider'
 
 export function App() {
   return (
@@ -45,7 +54,98 @@ export function App() {
           </p>
         </div>
 
-        <aside className='w-80'>
+        <aside className='w-80 space-y-6'>
+          <form className='space-y-6'>
+            <label
+              htmlFor='video'
+              className='border flex rounder-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary-foreground/5'
+            >
+              <FileVideo className='w-4 h-4' />
+              Select a video
+            </label>
+
+            <input
+              type='file'
+              id='video'
+              accept='video/mp4'
+              className='sr-only'
+            />
+
+            <Separator />
+
+            <div className='space-y-2'>
+              <Label htmlFor='transcription_prompt'>Transcription prompt</Label>
+              <Textarea
+                id='transcription_prompt'
+                className='h-20 leading-relaxed resize-none'
+                placeholder='Include keywords mentioned in the video separated by a comma (,)'
+              />
+            </div>
+
+            <Button type='submit' className='w-full'>
+              Upload video
+              <Upload className='w-4 h-4 ml-2' />
+            </Button>
+          </form>
+
+          <Separator />
+
+          <form className='space-y-6'>
+            <div className='space-y-2'>
+              <Label>Prompt</Label>
+
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder='Select a prompt...' />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value='title'>YouTube title</SelectItem>
+                  <SelectItem value='description'>YouTube description</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <span className='block text-xs text-muted-foreground italic'>
+                You'll be able to customize this option soon...
+              </span>
+            </div>
+
+            <div className='space-y-2'>
+              <Label>Template</Label>
+
+              <Select disabled defaultValue='gpt3.5'>
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value='gpt3.5'>GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <span className='block text-xs text-muted-foreground italic'>
+                You'll be able to customize this option soon...
+              </span>
+            </div>
+
+            <Separator />
+
+            <div className='space-y-4'>
+              <Label>Temp</Label>
+
+              <Slider min={0} max={1} step={0.1} className='cursor-pointer' />
+
+              <span className='block text-xs text-muted-foreground italic leading-relaxed'>
+                Higher values tend to make the result more creative and
+                error-prone.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type='submit' className='w-full'>
+              Run
+              <Wand2 className='w-4 h-4 ml-2 ' />
+            </Button>
+          </form>
         </aside>
       </main>
     </div>
